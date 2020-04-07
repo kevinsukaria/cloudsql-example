c6d86de46c2b24df793ab5cf346c24c52faeb54f
kevinsukaria <kevinsukaria@github.com>
2020-04-07 12:07:28 +0700

    tes

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import os
 
 from flask import Flask, render_template, request, Response
 import sqlalchemy
+
 # import pymysql
 
 
@@ -35,62 +36,60 @@ logger = logging.getLogger()
 # [START cloud_sql_mysql_sqlalchemy_create]
 # The SQLAlchemy engine will help manage interactions, including automatically
 # managing a pool of connections to your database
-db = sqlalchemy.create_engine(
-    # Equivalent URL:
-    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
-    sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=db_user,
-        password=db_pass,
-        database=db_name,
-        query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
-    ),
-    # ... Specify additional properties here.
-    # [START_EXCLUDE]
-    # [START cloud_sql_mysql_sqlalchemy_limit]
-    # Pool size is the maximum number of permanent connections to keep.
-    # pool_size=5,
-    # Temporarily exceeds the set pool_size if no connections are available.
-    # max_overflow=2,
-    # The total number of concurrent connections for your application will be
-    # a total of pool_size and max_overflow.
-    # [END cloud_sql_mysql_sqlalchemy_limit]
-    # [START cloud_sql_mysql_sqlalchemy_backoff]
-    # SQLAlchemy automatically uses delays between failed connection attempts,
-    # but provides no arguments for configuration.
-    # [END cloud_sql_mysql_sqlalchemy_backoff]
-    # [START cloud_sql_mysql_sqlalchemy_timeout]
-    # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-    # new connection from the pool. After the specified amount of time, an
-    # exception will be thrown.
-    # pool_timeout=30,  # 30 seconds
-    # [END cloud_sql_mysql_sqlalchemy_timeout]
-    # [START cloud_sql_mysql_sqlalchemy_lifetime]
-    # 'pool_recycle' is the maximum number of seconds a connection can persist.
-    # Connections that live longer than the specified amount of time will be
-    # reestablished
-    # pool_recycle=1800,  # 30 minutes
-    # [END cloud_sql_mysql_sqlalchemy_lifetime]
-    # [END_EXCLUDE]
-)
+# db = sqlalchemy.create_engine(
+#     # Equivalent URL:
+#     # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+#     sqlalchemy.engine.url.URL(
+#         drivername="mysql+pymysql",
+#         username=db_user,
+#         password=db_pass,
+#         database=db_name,
+#         query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
+#     ),
+# ... Specify additional properties here.
+# [START_EXCLUDE]
+# [START cloud_sql_mysql_sqlalchemy_limit]
+# Pool size is the maximum number of permanent connections to keep.
+# pool_size=5,
+# Temporarily exceeds the set pool_size if no connections are available.
+# max_overflow=2,
+# The total number of concurrent connections for your application will be
+# a total of pool_size and max_overflow.
+# [END cloud_sql_mysql_sqlalchemy_limit]
+# [START cloud_sql_mysql_sqlalchemy_backoff]
+# SQLAlchemy automatically uses delays between failed connection attempts,
+# but provides no arguments for configuration.
+# [END cloud_sql_mysql_sqlalchemy_backoff]
+# [START cloud_sql_mysql_sqlalchemy_timeout]
+# 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+# new connection from the pool. After the specified amount of time, an
+# exception will be thrown.
+# pool_timeout=30,  # 30 seconds
+# [END cloud_sql_mysql_sqlalchemy_timeout]
+# [START cloud_sql_mysql_sqlalchemy_lifetime]
+# 'pool_recycle' is the maximum number of seconds a connection can persist.
+# Connections that live longer than the specified amount of time will be
+# reestablished
+# pool_recycle=1800,  # 30 minutes
+# [END cloud_sql_mysql_sqlalchemy_lifetime]
+# [END_EXCLUDE]
+# )
 # [END cloud_sql_mysql_sqlalchemy_create]
 
-# socket = '/cloudsql/tweets-streamer:asia-east2:tes'
-# konek = pymysql.connect(
-#         # host='35.220.176.195',
-#         unix_socket=socket,
-#         user='root',
-#         password='123qweasd',
-#         database='tweets'
-#     )
-
-
+socket = '/cloudsql/tweets-streamer:asia-east2:tes'
+konek = pymysql.connect(
+    # host='35.220.176.195',
+    unix_socket=socket,
+    user='root',
+    password='123qweasd',
+    database='tweets'
+)
 
 
 @app.before_first_request
 def create_tables():
     # Create tables (if they don't already exist)
-    with db.connect() as conn:
+    with konek.cursor() as conn:
         conn.execute(
             "CREATE TABLE IF NOT EXISTS votes "
             "( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL, "
@@ -101,11 +100,12 @@ def create_tables():
 @app.route("/", methods=["GET"])
 def index():
     votes = []
-    with db.connect() as conn:
+    with konek.cursor() as conn:
         # Execute the query and fetch all results
-        recent_votes = conn.execute(
+        conn.execute(
             "SELECT candidate, time_cast FROM votes " "ORDER BY time_cast DESC LIMIT 5"
-        ).fetchall()
+        )
+        recent_votes = conn.fetchall()
         # Convert the results into a list of dicts representing votes
         for row in recent_votes:
             votes.append({"candidate": row[0], "time_cast": row[1]})
@@ -114,10 +114,12 @@ def index():
             "SELECT COUNT(vote_id) FROM votes WHERE candidate=:candidate"
         )
         # Count number of votes for tabs
-        tab_result = conn.execute(stmt, candidate="TABS").fetchone()
+        conn.execute(stmt, candidate="TABS")
+        tab_result = conn.fetchone()
         tab_count = tab_result[0]
         # Count number of votes for spaces
-        space_result = conn.execute(stmt, candidate="SPACES").fetchone()
+        conn.execute(stmt, candidate="SPACES")
+        space_result = conn.fetchone()
         space_count = space_result[0]
 
     return render_template(
@@ -153,7 +155,7 @@ def save_vote():
         return Response(
             status=500,
             response="Unable to successfully cast vote! Please check the "
-            "application logs for more details.",
+                     "application logs for more details.",
         )
         # [END_EXCLUDE]
     # [END cloud_sql_mysql_sqlalchemy_connection]

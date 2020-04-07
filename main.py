dd6dadc6017ca9dcd59b6402e3eec353d7dd2f1b
kevinsukaria <kevinsukaria@github.com>
2020-04-07 11:17:19 +0700

    tes

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import os
 
 from flask import Flask, render_template, request, Response
 import sqlalchemy
+import pymysql
 
 
 # Remember - storing secrets in plaintext is potentially unsafe. Consider using
@@ -34,23 +35,23 @@ logger = logging.getLogger()
 # [START cloud_sql_mysql_sqlalchemy_create]
 # The SQLAlchemy engine will help manage interactions, including automatically
 # managing a pool of connections to your database
-db = sqlalchemy.create_engine(
+# db = sqlalchemy.create_engine(
     # Equivalent URL:
     # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
-    sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=db_user,
-        password=db_pass,
-        database=db_name,
-        query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
-    ),
+    # sqlalchemy.engine.url.URL(
+    #     drivername="mysql+pymysql",
+    #     username=db_user,
+    #     password=db_pass,
+    #     database=db_name,
+    #     query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
+    # ),
     # ... Specify additional properties here.
     # [START_EXCLUDE]
     # [START cloud_sql_mysql_sqlalchemy_limit]
     # Pool size is the maximum number of permanent connections to keep.
-    pool_size=5,
+    # pool_size=5,
     # Temporarily exceeds the set pool_size if no connections are available.
-    max_overflow=2,
+    # max_overflow=2,
     # The total number of concurrent connections for your application will be
     # a total of pool_size and max_overflow.
     # [END cloud_sql_mysql_sqlalchemy_limit]
@@ -62,23 +63,34 @@ db = sqlalchemy.create_engine(
     # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
     # new connection from the pool. After the specified amount of time, an
     # exception will be thrown.
-    pool_timeout=30,  # 30 seconds
+    # pool_timeout=30,  # 30 seconds
     # [END cloud_sql_mysql_sqlalchemy_timeout]
     # [START cloud_sql_mysql_sqlalchemy_lifetime]
     # 'pool_recycle' is the maximum number of seconds a connection can persist.
     # Connections that live longer than the specified amount of time will be
     # reestablished
-    pool_recycle=1800,  # 30 minutes
+    # pool_recycle=1800,  # 30 minutes
     # [END cloud_sql_mysql_sqlalchemy_lifetime]
     # [END_EXCLUDE]
-)
+# )
 # [END cloud_sql_mysql_sqlalchemy_create]
+
+socket = '/cloudsql/tweets-streamer:asia-east2:tes'
+konek = pymysql.connect(
+        # host='35.220.176.195',
+        unix_socket=socket,
+        user='root',
+        password='123qweasd',
+        database='tweets'
+    )
+
+
 
 
 @app.before_first_request
 def create_tables():
     # Create tables (if they don't already exist)
-    with db.connect() as conn:
+    with konek.cursor() as conn:
         conn.execute(
             "CREATE TABLE IF NOT EXISTS votes "
             "( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL, "
@@ -89,7 +101,7 @@ def create_tables():
 @app.route("/", methods=["GET"])
 def index():
     votes = []
-    with db.connect() as conn:
+    with konek.cursor() as conn:
         # Execute the query and fetch all results
         recent_votes = conn.execute(
             "SELECT candidate, time_cast FROM votes " "ORDER BY time_cast DESC LIMIT 5"
@@ -131,7 +143,7 @@ def save_vote():
     try:
         # Using a with statement ensures that the connection is always released
         # back into the pool at the end of statement (even if an error occurs)
-        with db.connect() as conn:
+        with konek.cursor() as conn:
             conn.execute(stmt, time_cast=time_cast, candidate=team)
     except Exception as e:
         # If something goes wrong, handle the error in this section. This might
